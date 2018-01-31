59fe8cd956a342cb4ed00e5fef5cb96e026e6ee6
Diego Angulo <diego@MacBook-Pro-de-Diego.local>
2018-01-30 23:55:01 -0500

    bd production

--- dtesis/settings.py
+++ dtesis/settings.py
@@ -82,13 +82,24 @@ WSGI_APPLICATION = 'dtesis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dtesis2',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dtesis2',
+        'NAME': 'dtesis',
         'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
+        'PASSWORD': 'postgres',
+        'HOST': 'dtesis.cfoay7ddgrgf.us-east-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
