120d9c2c8e0c1622048732f8a4d183087b3603e9
Diego Angulo <d.angulo@adateceu.com>
2019-03-31 14:38:34 -0500

    Change to DB Postgres

--- dtesis/settings.py
+++ dtesis/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import pymysql
 
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -84,27 +86,27 @@ WSGI_APPLICATION = 'dtesis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dtesis2',
-#         'USER': 'postgres',
-#         'PASSWORD': '123456',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dtesis',
-        'USER': 'super',
+        'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'cardiocare-664.postgres.pythonanywhere-services.com',
-        'PORT': '10664',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'dtesis',
+#         'USER': 'developer',
+#         'PASSWORD': 'd3v3l0p3r',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
